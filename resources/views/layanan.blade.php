0e276abb7fad23e7642756beab81ca5fb39ba04b
Fajar Arrohman Nur Sahab <fajarsahab057@gmail.com>
2025-02-27 09:58:35 +0700

    Initial Commit

---
+++ resources/views/layanan.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <div class="py-6">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <div class="p-6 font-bold">
+                {{ __("Layanan Kami") }}
+            </div>
+        </h2>       
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
